f247342ca4c1d69cfbb18afc211ec9b94640a5c7
dracoder0002 <dracoder0002@gmail.com>
2026-06-25 15:47:01 -0400

    Add variables and console logs to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Dr. A";
+let school = "CPCC";
+var fun_fact = "I am a professor and professional sneaker head";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${fun_fact}`);
