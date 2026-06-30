fefd89293b4834cc2157df314c436db95b1455c9
dracoder0002 <dracoder0002@gmail.com>
2026-06-30 16:16:02 -0400

    Replace static variables with user prompts

--- jsdemo.js
+++ jsdemo.js
@@ -1,9 +1,9 @@
 console.log("This is loaded from the external JS file");
 alert("This is an alert!! Click to continue.");
 
-var name = "Dr. A";
-let school = "CPCC";
-var fun_fact = "I am a professor and professional sneaker head";
+var name = prompt("What is your name?");
+let school = prompt("What school do you attend?");
+var fun_fact = prompt("What should people know about you?");
 
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
