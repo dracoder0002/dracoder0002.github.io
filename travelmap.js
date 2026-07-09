c20c54e031313ea4913bb4a32bbf1b822428957d
dracoder0002 <dracoder0002@gmail.com>
2026-07-09 16:12:34 -0400

    Add marker for The Muraka restaurant on the map

--- travelmap.js
+++ travelmap.js
@@ -4,6 +4,13 @@ var popupcode = `<center>
 <h3>Hello from the Maldives!</h3>
 <iframe width="200" height="100" src="https://www.youtube.com/embed/1fT9pCa6r5M?si=_TVKeeNP6oKpIF_T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </center>`;
+var marker1 = [3.61738, 72.71727];
+var markercode = `<center>
+<h3>The Muraka & Ithaa Undersea Restaurant</h3>
+<a href="travel.jpg">
+<img src="travel.jpg" title="The Muraka & Ithaa Undersea Restaurant" height="100" width="200">
+</a>
+</center>`
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -14,3 +21,4 @@ L.circle(coordinates, {"title": "Hello from the Maldives!",
                       fillColor: '#e6ae17',
                       fillOpacity: 0.8,
                       radius: 90}).bindPopup(popupcode).addTo(map);
+L.marker(marker1).bindPopup(markercode).addTo(map);
