b847a307e7cdba129460754ab2be44e3efc65da8
dracoder0002 <dracoder0002@gmail.com>
2026-07-09 13:03:08 -0400

    Update popup content for CPCC marker

--- basicmap.js
+++ basicmap.js
@@ -1,20 +1,11 @@
 // 35.2271° N latitude and -80.8431° W longitude
 var charlotte = [35.2271, -80.8431];
 var cpcc = [35.2176665, -80.8314732];
-var popupContent = `
-    <div style="width: 300px; height: 250px;">
-        <iframe 
-            src="https://www.cpcc.edu" 
-            width="100%" 
-            height="100%" 
-            frameborder="0" 
-            style="border:none;">
-        </iframe>
-    </div>
-`;
 var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var cpccmarker = L.marker(cpcc, {title: "CPCC Central Campus"}).bindPopup(popupContent).addTo(map);
+var cpccmarker = L.marker(cpcc, {title: "CPCC Central Campus"}).bindPopup(
+    "<p>CPCC Central Campus</p><br><a href='https://www.cpcc.edu'>Visit cpcc.edu</a>"
+).addTo(map);
